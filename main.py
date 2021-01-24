1e673447dbceee658f08e72bcaad30a983b0904f
adrien-blanc <adrien.blanc74@outlook.fr>
2021-01-24 12:13:01 +0100

    AutoSave BDD

--- main.py
+++ main.py
@@ -376,6 +376,113 @@ async def unmute(ctx):
     with open(os.getenv('USER_JSON'),"w") as f:
             json.dump(users,f)
 
+#------------------------------------------------#
+#                                                #
+#                    LootBox                     #
+#                                                #
+#------------------------------------------------#
+
+@client.command(aliases = ["box"])
+async def lootBox(ctx):
+  user = ctx.author
+  users = await get_bank_data()
+
+  embed = discord.Embed(title = "Loot Disponible", description = "Coût de la LootBox : **250 Coins**", color = 3426654)
+  embed.add_field(name = "100 Coins", value = "33.5%", inline=True)
+  embed.add_field(name = "0 Coins", value = "20%", inline=True)
+  embed.add_field(name = "500 Coins", value = "15%", inline=True)
+  embed.add_field(name = "1 000 Coins", value = "5%", inline=True)
+  embed.add_field(name = "Random Coins [0 - 2500]", value = "3%", inline=True)
+  embed.add_field(name = "Random Coins [0 - 10000]", value = "1%", inline=True)
+  embed.add_field(name = "10 000 Coins", value = "1%", inline=True)
+  
+  embed.add_field(name = "Banque x1.05", value = "5%", inline=True)
+  embed.add_field(name = "Banque x1.1", value = "2.5%", inline=True)
+  embed.add_field(name = "Banque x1.25", value = "1%", inline=True)
+  embed.add_field(name = "Banque x0.8", value = "5%", inline=True)
+  embed.add_field(name = "Banque x0.5", value = "2.5%", inline=True)
+  embed.add_field(name = "Banque = 0", value = "0.5%", inline=True)
+  embed.add_field(name = "FreeLoot Box", value = "5%", inline=True)
+  embed.set_footer(text = f"Cocher ✅ pour jouer !")
+  message = await ctx.send(embed = embed)
+  await message.add_reaction("✅")
+
+  def checkEmoji(reaction, user):
+      return ctx.message.author == user and message.id == reaction.message.id and str(reaction.emoji) == "✅"
+  
+  def check(m):
+      return m.author == ctx.author and m.channel == ctx.channel
+
+  reaction, user = await client.wait_for("reaction_add", check = checkEmoji)
+  if reaction.emoji == "✅":
+    userWallet = users[str(user.id)]["wallet"]
+    if userWallet < 250:
+      await ctx.send(f"({user.name}) | Vous n'avez pas assez de Coins. ({userWallet}/250)")
+    else:
+
+      number = random.randrange(1000)
+      number += 1
+
+      users[str(user.id)]["wallet"] -= 250
+      with open(os.getenv('USER_JSON'),"w") as f:
+            json.dump(users,f)
+
+      if 1 <= number < 335:  # 100 Coins
+        await ctx.send(f"({user.name}) | Vous avez gagné **100  Coins**.")
+        users[str(user.id)]["wallet"] += 100
+      elif 335 <= number < 535: # 0 Coins
+        await ctx.send(f"({user.name}) | Vous avez gagné **0  Coins**.")
+      elif 535 <= number < 685: # 500 Coins
+        await ctx.send(f"({user.name}) | Vous avez gagné **500  Coins**.")
+        users[str(user.id)]["wallet"] += 500
+      elif 685 <= number < 735: # 1 000 Coins
+        await ctx.send(f"({user.name}) | Vous avez gagné **1 000  Coins**.")
+        users[str(user.id)]["wallet"] += 1000
+      elif 735 <= number < 745: # random [0 - 10 000] Coins
+        rand = random.randrange(10000)
+        rand += 1
+        await ctx.send(f"({user.name}) | Vous avez gagné un nombre random **[0 - 10 000] Coins**.")
+        await ctx.send(f"Résultat : **{rand} Coins**.")
+        users[str(user.id)]["wallet"] += int(rand)
+      elif 745 <= number < 775: # random [0 - 2 500] Coins
+        rand = random.randrange(2500)
+        rand += 1
+        await ctx.send(f"({user.name}) | Vous avez gagné un nombre random **[0 - 2 500] Coins**.")
+        await ctx.send(f"Résultat : **{rand} Coins**.")
+        users[str(user.id)]["wallet"] += int(rand)
+      elif 775 <= number < 785: # 10 000 Coins
+        await ctx.send(f"({user.name}) | Vous avez gagné **10 000  Coins**.")
+        users[str(user.id)]["wallet"] += 10000
+      elif 785 <= number < 835: # Banque x 1.05
+        await ctx.send(f"({user.name}) | Votre banque a été multipliée par **1.05**.")
+        users[str(user.id)]["wallet"] += 250
+        users[str(user.id)]["bank"] =  ceil(users[str(user.id)]["bank"]*1.05)
+      elif 835 <= number < 860: # Banque x 1.1
+        await ctx.send(f"({user.name}) | Votre banque a été multipliée par **1.1**.")
+        users[str(user.id)]["wallet"] += 250
+        users[str(user.id)]["bank"] =  ceil(users[str(user.id)]["bank"]*1.1)
+      elif 860 <= number < 870: # Banque x 1.25
+        await ctx.send(f"({user.name}) | Votre banque a été multipliée par **1.25**.")
+        users[str(user.id)]["wallet"] += 250
+        users[str(user.id)]["bank"] =  ceil(users[str(user.id)]["bank"]*1.25)
+      elif 870 <= number < 875: # Banque x 0
+        await ctx.send(f"({user.name}) | Votre banque a été multipliée par **0**.")
+        users[str(user.id)]["wallet"] += 250
+        users[str(user.id)]["bank"] =  ceil(users[str(user.id)]["bank"]*0)
+      elif 875 <= number < 900: # Banque x 0.5
+        await ctx.send(f"({user.name}) | Votre banque a été multipliée par **0.5**.")
+        users[str(user.id)]["wallet"] += 250
+        users[str(user.id)]["bank"] =  int(users[str(user.id)]["bank"]/2)
+      elif 900 <= number < 950: # Banque x 0.8
+        await ctx.send(f"({user.name}) | Votre banque a été multipliée par **0.8**.")
+        users[str(user.id)]["wallet"] += 250
+        users[str(user.id)]["bank"] =  int(users[str(user.id)]["bank"]/1.25)
+      elif 950 <= number < 1000: #FreeLoot Box
+        await ctx.send(f"({user.name}) | LootBox free.")
+        users[str(user.id)]["wallet"] += 250
+      with open(os.getenv('USER_JSON'),"w") as f:
+            json.dump(users,f)
+
 
 #------------------------------------------------#
 #                                                #
@@ -691,7 +798,7 @@ async def on_ready():
 
 #------------------------------------------------#
 #                                                #
-#           Timer point + Reset BEG              #
+#                  Timer Action                  #
 #                                                #
 #------------------------------------------------#
 
@@ -741,6 +848,70 @@ async def giveW():
   prize = "500"
 
   embed = discord.Embed(title = "Giveaway journalier!", description = f"{prize} Coins", color = 1752220)
+  embed.set_footer(text = f"Se termine dans {duree} à partir de maintenant !")
+  my_msg = await channel.send(embed = embed)
+
+  await my_msg.add_reaction("✅")
+
+  await asyncio.sleep(time)
+
+  new_msg = await channel.fetch_message(my_msg.id)
+
+  users = await new_msg.reactions[0].users().flatten()
+  users.pop(users.index(client.user))
+  winner = random.choice(users)
+
+  await channel.send(f"Résultat du Giveaway : Bravo à {winner.mention} qui a gagné {prize} Coins !")
+  
+  users = await get_bank_data()
+
+  users[str(winner.id)]["wallet"] += int(prize)
+  with open(os.getenv('USER_JSON'),"w") as f:
+      json.dump(users,f)
+
+@aiocron.crontab('0 21 * * 0')
+async def giveWeek():
+  users = await get_bank_data()
+
+  duree = "59m"
+
+  channel = client.get_channel(637209521202266124)
+  time = convert(duree)      
+  prize = "1000"
+
+  embed = discord.Embed(title = "Giveaway Dominical !", description = f"{prize} Coins", color = 1752220)
+  embed.set_footer(text = f"Se termine dans {duree} à partir de maintenant !")
+  my_msg = await channel.send(embed = embed)
+
+  await my_msg.add_reaction("✅")
+
+  await asyncio.sleep(time)
+
+  new_msg = await channel.fetch_message(my_msg.id)
+
+  users = await new_msg.reactions[0].users().flatten()
+  users.pop(users.index(client.user))
+  winner = random.choice(users)
+
+  await channel.send(f"Résultat du Giveaway : Bravo à {winner.mention} qui a gagné {prize} Coins !")
+  
+  users = await get_bank_data()
+
+  users[str(winner.id)]["wallet"] += int(prize)
+  with open(os.getenv('USER_JSON'),"w") as f:
+      json.dump(users,f)
+
+@aiocron.crontab('0 20 1 * *')
+async def giveMouth():
+  users = await get_bank_data()
+
+  duree = "59m"
+
+  channel = client.get_channel(637209521202266124)
+  time = convert(duree)      
+  prize = "5000"
+
+  embed = discord.Embed(title = "Giveaway Mensuel !", description = f"{prize} Coins", color = 1752220)
   embed.set_footer(text = f"Se termine dans {duree} à partir de maintenant !")
   my_msg = await channel.send(embed = embed)
 
